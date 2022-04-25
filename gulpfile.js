befd97f6360f060ef9c2a828dadd5944fd45a3fc
Zed0x01 <sherifashrafmedia@gmail.com>
2022-04-25 10:19:38 +0200

    V1

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var concat = require('gulp-concat');
+var prefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass')(require('sass'));
+var cleanCss =require('gulp-clean-css');
+
+
+gulp.task('html',()=>{
+    return gulp.src('./src/pug/components/index.pug')
+                .pipe(pug({pretty:true}))
+                .pipe(gulp.dest('./dist/'))
+})
+gulp.task('css',()=>{
+    return gulp.src('./src/sass/main.scss')
+            .pipe(sass({outputStyle:'compressed'}))
+            .pipe(prefixer('last 2 versions'))
+            .pipe(concat('main.css'))
+            .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('watch',()=>{
+    gulp.watch('./src/pug/**/*.*',gulp.series('html'));
+    gulp.watch('./src/sass/**/*.*',gulp.series('css'));
+})
