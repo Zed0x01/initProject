1d3b790e572ac2a165f61edd9d68bf484a8e0deb
Zed0x01 <sherifashrafmedia@gmail.com>
2022-04-27 08:52:15 +0200

    Update Sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,40 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var concat = require('gulp-concat');
-var prefixer = require('gulp-autoprefixer');
-var sass = require('gulp-sass')(require('sass'));
-var cleanCss =require('gulp-clean-css');
-
-const {parallel} = require('gulp');
-
-function html(){
-    return gulp.src('./src/pug/components/index.pug')
-                .pipe(pug({pretty:true}))
-                .pipe(gulp.dest('./dist/'))
+var gulp = require("gulp");
+var pug = require("gulp-pug");
+var concat = require("gulp-concat");
+var prefixer = require("gulp-autoprefixer");
+var sass = require("gulp-sass")(require("sass"));
+var cleanCss = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
+const { parallel, watch } = require("gulp");
+function html() {
+  return gulp
+    .src("./src/pug/components/index.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("./dist/"));
 }
 
-function css(){
-    return gulp.src('./src/sass/main.scss')
-            .pipe(sass({outputStyle:'compressed'}))
-            .pipe(prefixer('last 2 versions'))
-            .pipe(concat('main.css'))
-            .pipe(gulp.dest('./dist/'))
+function css() {
+  return gulp
+    .src("./src/sass/main.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(prefixer("last 2 versions"))
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest("./dist/"));
 }
 
-gulp.task('watch',()=>{
-    gulp.watch('./src/pug/**/*.*',gulp.series('html'));
-    gulp.watch('./src/sass/**/*.*',gulp.series('css'));
-})
-exports.default = parallel(html,css);
+function js() {
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist"));
+}
+
+// watch(['./src/**/*.*'],[css,html]);
+
+gulp.task("watch", () => {
+  gulp.watch("./src/pug/**/*.*", html);
+  gulp.watch("./src/sass/**/*.*", css);
+  gulp.watch("./src/js/*.js", js);
+});
+exports.default = parallel(html, css, js);
---
+++ src/js/project.js
@@ -0,0 +1,34 @@
+let navbar = document.getElementById("headerContainer");
+let navbarSec = document.getElementById("headerSec");
+let elements = document.querySelectorAll("[data-parralex]");
+
+window.onscroll = () => {
+  const navbarOffset = navbar.offsetTop;
+  if (window.pageYOffset >= navbarOffset + 145) {
+    navbar.classList.add("fixed");
+    navbarSec.style.backgroundColor = "white";
+    navbar.style.backgroundColor = "transparent";
+  } else {
+    navbar.classList.remove("fixed");
+    navbarSec.style.backgroundColor = "#f1f5fd";
+    navbar.style.backgroundColor = "#f1f5fd";
+  }
+};
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+window.addEventListener("scroll", () => {
+  elements.forEach((element) => {
+    const anim = element.getAttribute("animates");
+    if (isInViewport(element)) {
+      element.classList.add(anim);
+    }
+  });
+});
