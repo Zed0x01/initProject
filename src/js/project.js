117b1a3ee51c4884b9a7b3991e9ac5cc2f535af2
Zed0x01 <sherifashrafmedia@gmail.com>
2022-04-28 04:34:28 +0200

    Using Prettier

--- src/js/project.js
+++ src/js/project.js
@@ -20,9 +20,8 @@ function isInViewport(el) {
     rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= 
-        (window.innerWidth || document.documentElement.clientWidth)
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 window.addEventListener("scroll", () => {
